66ba70b5aaac6ca13d60dfd12679b87f6ca2d5e0
Codefuse <developer.codefuse@gmail.com>
2016-01-18 23:34:55 +1100

    Test 1

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -1,11 +1,4 @@
-
-for i in $(find .\packages -maxdepth 1 -mindepth 1 -type d -printf '%p\t%d\n' | sort -n -k2 | cut -f 1); do
-		echo -e "\n";
-		echo -e "\033[1m"+$i+"\033[0m";
-		cd $i;
-
-		VELOCITY_TEST_PACKAGES=1 meteor test-packages --velocity ./
-		cd ..;
-done
-
-
+cd packages/manga-core
+VELOCITY_TEST_PACKAGES=1 meteor test-packages --velocity ./
+cd packages/manga-users
+VELOCITY_TEST_PACKAGES=1 meteor test-packages --velocity ./
