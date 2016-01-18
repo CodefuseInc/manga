b446824a6681956c90e3c02de8658ee626ec9d32
Codefuse <developer.codefuse@gmail.com>
2016-01-18 23:38:12 +1100

    test2

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -1,4 +1,9 @@
+pwd 
+
 cd packages/manga-core
 VELOCITY_TEST_PACKAGES=1 meteor test-packages --velocity ./
+pwd
+
 cd packages/manga-users
 VELOCITY_TEST_PACKAGES=1 meteor test-packages --velocity ./
+pwd
